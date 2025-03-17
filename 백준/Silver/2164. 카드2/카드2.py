34b19eb33cee08b00eaa96f2ccbcf46e8c0f5c72
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-03-17 16:16:15 +0900

    [Silver IV] Title: 카드2, Time: 192 ms, Memory: 50968 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,10 @@
+from collections import deque
+
+N = int(input())
+num_list = deque(range(1,N+1))
+
+while len(num_list) != 1 :
+    num_list.popleft()
+    num_list.append(num_list.popleft())
+
+print(num_list.pop())
