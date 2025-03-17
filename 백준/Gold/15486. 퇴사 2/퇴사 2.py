4dfb81e51fedd961f6a6e751a34674292f6d85a4
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-03-17 20:45:47 +0900

    [Gold V] Title: 퇴사 2, Time: 2044 ms, Memory: 99424 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n+1)
+
+for i in range(1, n+1):
+    t, p = map(int, input().split())
+    dp[i] = max(dp[i-1], dp[i])
+    if i+t<=n+1:
+        dp[i+t-1] = max(dp[i-1]+p, dp[i+t-1])
+
+print(dp[-1])
