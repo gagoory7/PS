7e409f87d41b278a4b095cd39fd809ff32bb00de
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-03-18 18:12:18 +0900

    [Silver III] Title: 바이러스, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,17 +1,37 @@
-import sys
-
-input = sys.stdin.readline
+from collections import deque
 
 t = int(input())
 n = int(input())
 
-graphs = [ set(map(int,input().split())) for _ in range(n)]
 
-virus = set({1})
+graphs = [ []*(t+1) for _ in range(t+1)]
 
-for i in range(n) :
-    for graph in graphs :
-        if virus & graph :
-            virus = virus | graph
+for _ in range(n) :
+    a,b = map(int,input().split())
+    graphs[a].append(b)
+    graphs[b].append(a)
 
-print(len(virus) - 1)
+virus = 1
+
+visited = [0]*(t+1)
+count = 0
+
+def bfs(x) :
+    global count
+    visited[x] = 1
+    q = deque()
+
+    q.append(x)
+
+    while q :
+        target = q.popleft()
+
+        for i in graphs[target] :
+            if visited[i] == 0 :
+                visited[i] = 1
+                q.append(i)
+                count += 1
+    print(count)
+    
+
+bfs(virus)
