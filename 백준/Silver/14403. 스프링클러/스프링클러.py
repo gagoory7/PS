d1604da411cbb8da17f6150efcdab049841537be
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-04-05 21:46:10 +0900

    [Silver II] Title: 스프링클러, Time: 300 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/14403. 스프링클러/스프링클러.py
+++ 백준/Silver/14403. 스프링클러/스프링클러.py
@@ -1,26 +1,19 @@
 import sys
-
 input = sys.stdin.readline
 
 def solve():
     n, x1, y1, x2, y2 = map(int, input().split())
-    flowers = [list(map(int, input().split())) for _ in range(n)]
+    flowers = [tuple(map(int, input().split())) for _ in range(n)]
 
-    d1 = [((fx - x1)**2 + (fy - y1)**2) for fx, fy in flowers]
-    d2 = [((fx - x2)**2 + (fy - y2)**2) for fx, fy in flowers]
+    d1 = [(fx - x1) ** 2 + (fy - y1) ** 2 for fx, fy in flowers]
+    d2 = [(fx - x2) ** 2 + (fy - y2) ** 2 for fx, fy in flowers]
 
     ans = float('inf')
-    if n > 0:
-        ans = min(max(d1), max(d2))
 
-    for i in range(n):
-        r1_sq = d1[i]
-        r2_sq_needed = max((d2[j] for j in range(n) if d1[j] > r1_sq), default=0)
-        ans = min(ans, r1_sq + r2_sq_needed)
-
-        r2_sq = d2[i]
-        r1_sq_needed = max((d1[j] for j in range(n) if d2[j] > r2_sq), default=0)
-        ans = min(ans, r1_sq_needed + r2_sq)
+    for i in range(n + 1):  # i는 r1^2의 후보
+        r1_sq = 0 if i == n else d1[i]
+        r2_sq = max((d2[j] for j in range(n) if d1[j] > r1_sq), default=0)
+        ans = min(ans, r1_sq + r2_sq)
 
     print(ans)
 
