5bc18ae53e181fc22dd0076afc0ca1c13413b45a
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-04-09 23:36:14 +0900

    [Platinum V] Title: 버블 소트, Time: 2092 ms, Memory: 88880 KB -BaekjoonHub

---
+++ 백준/Platinum/1517. 버블 소트/버블 소트.py
@@ -0,0 +1,38 @@
+n = int(input())
+nums = list(map(int,input().split()))
+
+def count(arr):
+    n = len(arr)
+    if n <= 1:
+        return 0
+    
+    mid = n // 2
+    left = arr[:mid]
+    right = arr[mid:]
+    
+    inv_count = count(left) + count(right)
+    
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+            inv_count += len(left) - i
+        k += 1
+    
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+    
+    return inv_count
+
+print(count(nums))
