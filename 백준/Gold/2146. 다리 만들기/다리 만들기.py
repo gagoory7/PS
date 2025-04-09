1237bc953f9b8bae01e8517ce8b42dd9c5be9df3
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-04-09 11:01:07 +0900

    [Gold III] Title: 다리 만들기, Time: 1576 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/2146. 다리 만들기/다리 만들기.py
+++ 백준/Gold/2146. 다리 만들기/다리 만들기.py
@@ -1,73 +1,71 @@
-import sys
+
 from collections import deque
+import sys
 
 input = sys.stdin.readline
 
 n = int(input())
 
-graphs = [ list(map(int,input().split())) for _ in range(n) ]
+
+graphs = [ list(map(int,input().split())) for _ in range(n)]
 
 num = 2
 
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-# 처음 섬 구분 
-def bfs(x,y) :
+def bfs1(x,y) :
     global num
-    graphs[x][y] = num
     q = deque()
     q.append((x,y))
+    graphs[x][y] = num
 
     while q :
         x,y = q.popleft()
         for i in range(4) :
-            nx,ny = x+dx[i], y+dy[i]
-            if 0<=nx<n  and  0 <= ny < n :
-                if graphs[nx][ny] == 1 :
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0<=nx<n and 0<=ny<n :
+                if graphs[nx][ny] == 1:
                     q.append((nx,ny))
                     graphs[nx][ny] = num
-    num+=1
+            
+    num +=1
 
 for i in range(n) :
     for j in range(n) :
         if graphs[i][j] == 1 :
-            bfs(i,j)
+            bfs1(i,j)
 
+            
+def bfs2(num) :
+    q = deque()
 
+    dist = [[-1] * n for _ in range(n)]
+
+    for i in range(n) :
+        for j in range(n) :
+            if graphs[i][j] ==  num :
+                dist[i][j] = 0
+                q.append((i,j))
+
+    while q :
+        x, y = q.popleft() 
+        for i in range(4) :
+            nx, ny = x + dx[i], y + dy[i]
+
+            if 0<=nx<n and 0<=ny < n :
+
+                if graphs[nx][ny] != num and graphs[nx][ny] != 0 :
+                    return dist[x][y]
+                if graphs[nx][ny] == 0 and dist[nx][ny] == -1 :
+                    dist[nx][ny] = dist[x][y] +1
+                    q.append((nx,ny))
+   
+
+answer = 1e9
+
+for i in range(2,num) :
+    answer = min(answer,bfs2(i))
     
-answer = 1e8
-
-# 가장 가까운 섬 찾기 
-def bfs2(v):
-  q = deque()
-
-  # 거리이자 좌표가 갔는지
-  dist = [[-1]*n for _ in range(n)]
-
-  # 섬 별 첫 시작 좌표
-  for i in range(n):
-    for j in range(n):
-      if graphs[i][j]==v:
-        dist[i][j] = 0
-        q.append([i,j])
-
-  while q:
-    x, y = q.popleft()
-
-    for i in range(4) :
-       nx,ny = x + dx[i], y + dy[i]
-       if 0<=nx<n and 0<=ny<n :
-          if graphs[nx][ny] and graphs[nx][ny] != v:
-             return dist[x][y]
-          elif (not graphs[nx][ny]) and dist[nx][ny] == -1 :
-             dist[nx][ny] = dist[x][y] +1
-             q.append([nx,ny])
-
-  return int(1e9)
-                
-# 섬 들 숫자로 넣기 
-
-for v in range(2,num):
-  answer = min(answer, bfs2(v))
 print(answer)
