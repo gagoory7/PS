bcf1e4fcee9bee332d4d919d110086b31f5e575d
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-04-09 18:37:34 +0900

    [Silver I] Title: 경로 찾기, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -7,21 +7,23 @@ n = int(input())
 graphs = [ list(map(int,input().split())) for _ in range(n)]
 
 
-def dfs(start) :
-    for i in range(n) :
-        if graphs[start][i] == 1 and visited[i] == 0 :
-            visited[i] = 1
-            dfs(i)
+def dfs(start, visited):
+    for i in range(n):
+        if start == i :
+            continue
+
+        if not visited[i] and graphs[start][i]:
+            visited[i] = True
+            dfs(i, visited)
 
 
 
 for i in range(n) :
     visited = [False] * n
-    dfs(i)
+    dfs(i,visited)
     for j in range(n) :
         if visited[j] :
             graphs[i][j] = 1
     
 for graph in graphs :
     print(*graph)
-    
