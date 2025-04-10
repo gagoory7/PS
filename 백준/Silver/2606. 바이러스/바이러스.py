9cff75461388e1c61e933b25747a4e07a36e851d
gagoory7 <163074222+gagoory7@users.noreply.github.com>
2025-04-10 14:13:39 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,37 +1,33 @@
-from collections import deque
+import sys
 
-t = int(input())
-n = int(input())
+input = sys.stdin.readline
 
+t = int(input())        # 총 컴퓨터 수
+n = int(input())        # 연결된 쌍의 수
 
-graphs = [ []*(t+1) for _ in range(t+1)]
+# 그래프 초기화 (주의: []*(t+1)은 잘못된 방식)
+graphs = [[] for _ in range(t+1)]
 
-for _ in range(n) :
-    a,b = map(int,input().split())
+# 간선 정보 입력받기
+for _ in range(n):
+    a, b = map(int, input().split())
     graphs[a].append(b)
     graphs[b].append(a)
 
-virus = 1
-
-visited = [0]*(t+1)
+visited = [0] * (t+1)
 count = 0
 
-def bfs(x) :
+# DFS 정의
+def dfs(x):
     global count
     visited[x] = 1
-    q = deque()
+    for i in graphs[x]:
+        if not visited[i]:
+            count += 1
+            dfs(i)
 
-    q.append(x)
+# 1번 컴퓨터에서 시작
+dfs(1)
 
-    while q :
-        target = q.popleft()
-
-        for i in graphs[target] :
-            if visited[i] == 0 :
-                visited[i] = 1
-                q.append(i)
-                count += 1
-    print(count)
-    
-
-bfs(virus)
+# 결과 출력: 감염된 컴퓨터 수 (1번은 제외)
+print(count)
